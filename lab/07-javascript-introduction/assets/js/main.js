45f645a92ba7b34d0cb9857951c3305aaf521680
Vivian Sinong Li <vivianli@Vivians-MacBook-Pro.local>
2018-03-23 21:02:30 -0400

    08js

--- lab/07-javascript-introduction/assets/js/main.js
+++ lab/07-javascript-introduction/assets/js/main.js
@@ -91,7 +91,7 @@ console.log('Objects -');
 
 var me = {
   name: 'Sinong',
-  age: 19,
+  age: 3,
   nativeNewYorker: false
 };
 console.log('me:', me);
@@ -103,21 +103,81 @@ console.log('');
 // FUNCTIONS
 console.log('FUNCTIONS -');
 
-var sayHi = function() {
-  console.log('hi!')
-};
-console.log('sayHi():');
-sayHi();
-
-var saySomeMessage = function(cheese) {
-  console.log(cheese)
-};
-console.log("saySomeMessage('hello'):");
-saySomeMessage('hello');
-saySomeMessage('goodbye');
-
-var addNumbers = function(num1, num2) {
-  return num1 + num2
+var square = function (number) {
+  return number*number
 }
 
-console.log(addNumbers(8,12));
+var Natashia = {
+  name: 'Natashia',
+  age:'19',
+  nativeNewYorker:'false'
+}
+
+var describeMe = function (me){
+  return 'My name is ' + me.name + '!!!!' + 'I am ' + me.age + ' years old.' + ' If you ask me if I am a native New Yorker, my response would be:' + me.nativeNewYorker
+}
+
+console.log('')
+
+//CONDITIONAL STATEMENTS
+
+console.log('Conditional Statements -')
+
+if(me.age > 21){
+  console.log('party time')
+}else if (me.age < 5){
+  console.log('why are you here')
+}
+else{
+  console.log('not quite yet')
+
+}
+
+console.log('')
+
+
+//LOOP
+console.log('Loops -')
+
+var hamburgers = 0;
+for (var i = 0; hamburgers < 10; i++ ){
+  console.log(hamburgers + ' hamburger(s)')
+  hamburgers++;
+}
+
+for (var i = 0; i <= 6; i = i + 2){
+  console.log(i)
+}
+
+console.log('how many licks does it take to get to the center of a tootsie pop?')
+
+for(var licks = 1; licks <= 4; licks++){
+  if (licks ==4) {
+    console.log('CRUNCH!!!')
+  } else {
+    console.log(licks)
+  }
+}
+
+
+
+
+
+// var sayHi = function() {
+//   console.log('hi!')
+// };
+// console.log('sayHi():');
+// sayHi();
+//
+// var saySomeMessage = function(cheese) {
+//   console.log(cheese)
+// };
+// console.log("saySomeMessage('hello'):");
+// saySomeMessage('hello');
+// saySomeMessage('goodbye');
+//
+// var addNumbers = function(num1, num2) {
+//   return num1 + num2
+// }
+//
+// console.log(addNumbers(8,12));
